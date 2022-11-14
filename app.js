4109f95d63f751c4d5435e6f219d4fe1b5b3a749
Karizali <96954751+Karizali@users.noreply.github.com>
2022-11-14 17:09:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,268 @@
+//                              ALERT
+//                                  1
+alert("Welcome to our website");
+//                              End 1
+
+//                                  2
+alert("Error! Please enter a valid password.");
+//                              End 2
+
+//                                  3
+alert("Welcome to JS land \nHappy coding!");
+//                              End 3
+//                                  4
+alert("Welcome to JS Land...");
+//                              End 4
+//                                  5
+alert("Hello...I can run JS through my web browers's console")
+//                              End 5
+/*                                  6
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Resume</title>
+</head>
+<body>
+    <h1> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Syed Kariz <br> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Ali</h1>
+    <blockquote>
+        My name is Syed Kariz Ali. I am 20 years old. currently
+        , I am studying BE.SE at PAF-KIET university. I live in
+         Karachi. And I am doing web and app dev cource at Jawan Pakistan.
+         <br>Briefly state your career objective, or  summarize 
+         what makes you stand out. Use language from the job
+          description as keywords.
+    </blockquote>
+    <br>
+    <br>
+    <hr>
+    <b>&nbsp; Skills</b>
+    <ul>
+        <li><p>List your strenghts relevant for the role you're applying for</p></li>
+        <li><p>List one of your strenghts</p></li>
+        <li><p>List one of your strenghts</p></li>
+        <li><p>List one of your strenghts</p></li>
+        <li><p>List one of your strenghts</p></li>
+    </ul>
+    <hr>
+    <b>Experience</b>
+    <p>DATES:FROM-TO</p>
+    <h3 class="h3">Job Title/ <b class="b">Company, Location</b> </h3>
+    <p>Describe your responsibilities and achievements in terms of impact and results. Use examples,<br> but keep it short.</p>
+    <p>DATES:FROM-TO</p>
+    <h3 class="h3">Job Title/ <b class="b">Company, Location</b> </h3>
+    <p>Describe your responsibilities and achievements in terms of impact and results. Use examples,<br> but keep it short.</p>
+    <hr>
+    <b>Education</b>
+    <p>Month Year</p>
+    <h3 class="h3">Degree Title/ <b class="b">School, Location</b> </h3>
+    <p>It's okay to brag about your GPA, awards, and honors. Feel free to summarize your coursework too.</p>
+    <p>Month Year</p>
+    <h3 class="h3">Degree Title/ <b class="b">School, Location</b> </h3>
+    <p>It's okay to brag about your GPA, awards, and honors. Feel free to summarize your coursework too.</p>
+    <hr>
+    <b>Activities</b>
+    <p>Use this section to highlight your relevant passions, activities, and how you like to given back. It's good to <br>include Leadership and volunteer experience
+         here. Or show off important extras like <br> publications,certifications, languages and more.
+    </p>
+    <script>
+        alert("It's the CV for new job.")
+    </script>
+</body>
+</html>     
+                                End 6*/
+//                                  7
+// a. Head
+// 
+// <script>
+// alert("This is an alert!");
+// </script>
+// 
+// b. Body (before your page’s HTML)
+// 
+// <script>
+// alert("This is an alert!");
+// </script>
+// 
+// <!DOCTYPE html>
+// <html>
+// <head>
+// <title>Page Title</title>
+// </head>
+// <body>
+// 
+// c. Body (inside your page’s HTML)
+// 
+// <h1>This is a Heading</h1>
+// <p>This is a paragraph.</p>
+// 
+// <script>
+// alert("This is an alert!");
+// </script>
+// 
+// d. Body (after your page’s HTML)
+// 
+// </body>
+// </html>
+// 
+// <script>
+// alert("This is an alert!");
+// </script>
+//                                End 7
+                            //VARIABLES FOR STRINGS 
+                                    //1
+var username
+                                    //2
+var  myName='Syed Kariz Ali'
+                                    //3
+var message="Hello World"
+alert(message);
+                                    //4
+var name="Jhone Deo"
+var age="15 years old"
+var certifications="certified Mobile Application Development"
+alert(name);
+alert(age);
+alert(certifications);
+                                    //5
+var food="Pizza\nPizz\nPiz\nPi\nP"
+alert(food);
+                                    //6
+var  email="karizali310@gmail.com"
+alert("My email address is "+email);
+                                    //7
+var  book="A smarter way to learn JavaScript"
+alert("I am trying to learn from a book "+book);
+                                    //8
+document.write("Yah! I can write HTML content through JS.")
+                                    //9
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+                        //VARIABLES FOR NUMBERS
+//1
+var age=20;
+alert("I am "+age + "years old");
+//2
+var count = 14;
+
+if (count == 14) {
+  alert("You have visited this site " + count + " times.");
+}
+
+else {
+  alert("You have visited this site " + count + " times.");
+}
+//3
+var birthYear=2000
+document.write("I was born in "+birthYear+"\n Data type of given variable is number");
+//4
+var Visitorname=prompt();
+var Producttitle =prompt();
+var Quantity =prompt();
+document.write(Visitorname+" ordered "+Quantity+Producttitle+"on XYZ Clothing store");
+//                           VARIABLE NAMES: LEGAL & ILLEGAL 
+//1
+var a,b,c;
+//2
+// legal      var name_your,file,folder,computer,pro;
+// illegal    var name of person's,@email, app123 ,<input>,12345;
+//3
+document.write("Rules for naming JS variables\n Variable names can only contain and\n For example $my_1stVariable Variables must begin with a or .For example $name, _name or name Variable names are case Variable names should not be JS");
+
+//                                  MATH EXPRESSIONS
+//1
+// var a=prompt("enter a");
+// var b=prompt("enter b");
+// var c=a+b;
+// alert("sum of "+a+" and "+b+" is "+c);
+//2
+// var a=prompt("enter a");
+// var b=prompt("enter b");
+// var c=a-b;
+// alert("subtraction of "+a+" and "+b+" is "+c);
+
+// var a=prompt("enter a");
+// var b=prompt("enter b");
+// var c=a*b;
+// alert("multiplicatiop of "+a+" and "+b+" is "+c);
+
+// var a=prompt("enter a");
+// var b=prompt("enter b");
+// var c=a/b;
+// alert("Divison of "+a+" and "+b+" is "+c);
+//3
+var d="three";
+alert(d)
+d=3;
+e=d++
+alert("after increment "+e);
+f=e+7;
+alert("after addition "+f);
+g=f--
+alert("after decrement "+g);
+h=g%3;
+alert("after divison reminder is "+h);
+//4
+var price=600;
+var price5=price*5;
+alert("5 Ticket price is "+price5);
+//5
+for(i=0;i<10;i++){
+    console.log("2 X "+i +" = "+(i+1)*2);
+}
+//6
+var cal=prompt("Enter calcius ")
+var deg=(cal*(9/5)+32);
+alert("Temperature in fahrenheite is "+deg);
+var fhrn=prompt("Enter fahrenheit");
+var deg2=(fhrn-32*(5/9));
+alert("Temperature in calcius is "+deg2);
+//7
+var item1=100;
+var item1q=3;
+var item1t=item1*item1q;
+var item2=370;
+var item2q=5;
+var item2t=item2*item2q;
+var excharge=233;
+alert("Toatal price is "+(excharge+item1t+item2t));
+//8
+var toatal=850;
+var st1o=700;
+var st2o=560;
+var st1per=(st1o/toatal)*100;
+var st2per=(st2o/toatal)*100;
+alert("pertenages of Student 1 and 2 are "+st1per,st2per+"respectively");
+//9
+var dollar=10;
+var riyal=25;
+var totalc=(dollar*104.80)+(riyal*28);
+alert("Total amount is "+totalc);
+//10
+var ab=10;
+alert("Result is "+(((ab+5)*10))/2);
+//11
+var currentyear=2022;
+var birthyear=1994;
+alert("Your age is "+(currentyear-birthyear));
+//12
+var rad=22;
+alert("Circumference is "+2*3.142*rad);
+alert("Area of circle is "+3.142*(rad*rad));
+//13
+var fav="lays";
+var ageof=20;
+var maxage=70;
+var agv=250;
+alert("You wants "+((((maxage-ageof)*365)*agv)+fav)+"money For the rest of your life.");
+
+
+
+
+
+
+
+
